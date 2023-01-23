8cfd17ee6f0c0d15f32b1abae0163d1fab06991e
RomanKubay <romankubaj40@gmail.com>
2023-01-24 01:46:16 +0200

    modified:   config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 API_KEY = os.environ['API_KEY']
 MONGODB_HOST = os.environ['MONGODB_HOST']
+print("sdjlkgdskjpdjh!!!!!!!!!!!!!!!!")
+print(API_KEY)
+print(MONGODB_HOST)
 
 DEV_TG_NICKNAME = "thisRomana"
 leng_sort_word = 3
