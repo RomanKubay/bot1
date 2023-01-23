560da4074db49e87e868187cdf0f416fc0853a00
RomanKubay <romankubaj40@gmail.com>
2023-01-24 01:33:39 +0200

    modified:   config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-API_KEY = "1868877610:AAG_qkPZpTAy5nsuQdKCEp85SAOwN4MBY3s"
-MONGODB_HOST = "mongodb+srv://RomanKubay:7iunrPEfsDm8UAS8@cluster.myvigzq.mongodb.net/?retryWrites=true&w=majority"
+import os
+API_KEY = os.environ['API_KEY']
+MONGODB_HOST = os.environ['MONGODB_HOST']
+
 DEV_TG_NICKNAME = "thisRomana"
 leng_sort_word = 3
 max_history_leng = 100
